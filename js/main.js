bfade65d5d5a1ff47112af0941f2e1d5b0aa2e95
danhlogan <logan0839@student.cptc.edu>
2020-05-10 16:53:45 -0700

    Finished function isAllDataValid, minor tweaks, fixes, adjustments

--- js/main.js
+++ js/main.js
@@ -4,7 +4,7 @@ var PlayerCharacter = (function () {
     return PlayerCharacter;
 }());
 window.onload = function () {
-    var addBtn = document.querySelector("input[type=button]");
+    var addBtn = document.querySelector("button");
     addBtn.onclick = submitInfo;
 };
 function submitInfo() {
@@ -12,9 +12,88 @@ function submitInfo() {
         var pageInfo = getPageInfo();
         displayInfo(pageInfo);
     }
+    if (!isAllDataValid()) {
+    }
 }
 function isAllDataValid() {
-    return true;
+    var flag = true;
+    var vPlayerName = document.getElementById("playerName").value;
+    var vCharName = document.getElementById("charName").value;
+    var vcharLevel = parseInt(document.getElementById("charLevel").value);
+    var vRace = document.getElementById("race").value;
+    var vClass = document.getElementById("class").value;
+    var vStr = document.getElementById("str").value;
+    var vDex = document.getElementById("dex").value;
+    var vCon = document.getElementById("con").value;
+    var vInt = document.getElementById("int").value;
+    var vWis = document.getElementById("wis").value;
+    var vCharisma = document.getElementById("charisma").value;
+    if (vPlayerName == "") {
+        document.getElementById("nameSpan").innerText = "Please enter your name";
+        document.getElementById("playerNameFS").style.width = "350px";
+        flag = false;
+    }
+    if (vCharName == "") {
+        document.getElementById("charNameSpan").innerText = "Please decide name";
+        document.getElementById("charInfo").style.width = "500px";
+        flag = false;
+    }
+    if (vcharLevel < 1 || vcharLevel > 20 || document.getElementById("charLevel").value == "") {
+        document.getElementById("levelSpan").innerText = "Enter a number between 1 - 20";
+        document.getElementById("charInfo").style.width = "500px";
+        flag = false;
+    }
+    if (vRace == "Races") {
+        document.getElementById("raceSpan").innerText = "Please choose a race";
+        document.getElementById("charInfo").style.width = "500px";
+        flag = false;
+    }
+    if (vClass == "Character Classes") {
+        document.getElementById("classSpan").innerText = "Please choose a class";
+        document.getElementById("charInfo").style.width = "500px";
+        flag = false;
+    }
+    if (!document.querySelector('input[name="law"]:checked')) {
+        document.getElementById("lawSpan").innerText = "Please choose your Ethics";
+        document.getElementById("charInfo").style.width = "500px";
+        flag = false;
+    }
+    if (!document.querySelector('input[name="align"]:checked')) {
+        document.getElementById("alignSpan").innerText = "Please choose your Morals";
+        document.getElementById("charInfo").style.width = "500px";
+        flag = false;
+    }
+    if (vStr == "") {
+        document.getElementById("strSpan").innerText = "Enter number 8 - 20";
+        document.getElementById("abilScores").style.width = "350px";
+        flag = false;
+    }
+    if (vDex == "") {
+        document.getElementById("dexSpan").innerText = "Enter number 8 - 20";
+        document.getElementById("abilScores").style.width = "350px";
+        flag = false;
+    }
+    if (vCon == "") {
+        document.getElementById("conSpan").innerText = "Enter number 8 - 20";
+        document.getElementById("abilScores").style.width = "350px";
+        flag = false;
+    }
+    if (vInt == "") {
+        document.getElementById("intSpan").innerText = "Enter number 8 - 20";
+        document.getElementById("abilScores").style.width = "350px";
+        flag = false;
+    }
+    if (vWis == "") {
+        document.getElementById("wisSpan").innerText = "Enter number 8 - 20";
+        document.getElementById("abilScores").style.width = "350px";
+        flag = false;
+    }
+    if (vCharisma == "") {
+        document.getElementById("charismaSpan").innerText = "Enter number 8 - 20";
+        document.getElementById("abilScores").style.width = "350px";
+        flag = false;
+    }
+    return flag;
 }
 function getPageInfo() {
     var form = new PlayerCharacter();
@@ -33,5 +112,5 @@ function getPageInfo() {
     form.charisma = parseInt(document.getElementById("charisma").value);
     return form;
 }
-function displayInfo(pageInfo) {
+function displayInfo(form) {
 }
--- ts/main.ts
+++ ts/main.ts
@@ -13,17 +13,16 @@ class PlayerCharacter
     int: number;
     wis: number;
     charisma: number;
-
 }
 
 window.onload = function()
 {
-    let addBtn = <HTMLElement> document.querySelector("input[type=button]")
+    let addBtn = <HTMLElement> document.querySelector("button")
     addBtn.onclick = submitInfo
 }
 
 // Main  
-function submitInfo() 
+function submitInfo():void 
 {
     if(isAllDataValid())
     {
@@ -31,19 +30,128 @@ function submitInfo()
         displayInfo(pageInfo)
     }
 
+    if(!isAllDataValid())
+    {
+        //alert("TEST!")
+    }
 }
 
 // Validates form 
-function isAllDataValid()
+function isAllDataValid():boolean
 {
-    return true;
+        let flag = true;
+
+        let vPlayerName = (<HTMLInputElement> document.getElementById("playerName")).value
+        let vCharName = (<HTMLInputElement> document.getElementById("charName")).value
+        let vcharLevel = parseInt((<HTMLInputElement> document.getElementById("charLevel")).value)
+        let vRace = (<HTMLSelectElement> document.getElementById("race")).value
+        let vClass = (<HTMLSelectElement> document.getElementById("class")).value
+        let vStr = (<HTMLInputElement> document.getElementById("str")).value
+        let vDex = (<HTMLInputElement> document.getElementById("dex")).value
+        let vCon = (<HTMLInputElement> document.getElementById("con")).value
+        let vInt = (<HTMLInputElement> document.getElementById("int")).value
+        let vWis = (<HTMLInputElement> document.getElementById("wis")).value
+        let vCharisma = (<HTMLInputElement> document.getElementById("charisma")).value
+        
+        if(vPlayerName == "")
+        {
+            document.getElementById("nameSpan").innerText = "Please enter your name"
+            document.getElementById("playerNameFS").style.width = "350px"
+            flag = false
+        }
+
+        if(vCharName == "")
+        {
+            document.getElementById("charNameSpan").innerText = "Please decide name"
+            document.getElementById("charInfo").style.width = "500px"
+            flag = false
+        }
+
+        if(vcharLevel < 1 || vcharLevel > 20 || document.getElementById("charLevel").value == "")
+        {
+            document.getElementById("levelSpan").innerText = "Enter a number between 1 - 20" 
+            document.getElementById("charInfo").style.width = "500px"
+            flag = false
+        }
+
+        if(vRace == "Races")
+        {
+            document.getElementById("raceSpan").innerText = "Please choose a race" 
+            document.getElementById("charInfo").style.width = "500px"
+            flag = false
+        }
+
+        if(vClass == "Character Classes")
+        {
+            document.getElementById("classSpan").innerText = "Please choose a class" 
+            document.getElementById("charInfo").style.width = "500px"
+            flag = false
+        }
+
+        if(!(<HTMLInputElement> document.querySelector('input[name="law"]:checked')))
+        {
+            document.getElementById("lawSpan").innerText = "Please choose your Ethics"
+            document.getElementById("charInfo").style.width = "500px"
+            flag = false
+        }
+
+        if(!(<HTMLInputElement> document.querySelector('input[name="align"]:checked')))
+        {
+            document.getElementById("alignSpan").innerText = "Please choose your Morals"
+            document.getElementById("charInfo").style.width = "500px"
+            flag = false 
+        }
+
+        if(vStr == "")
+        {
+            document.getElementById("strSpan").innerText = "Enter number 8 - 20"
+            document.getElementById("abilScores").style.width = "350px"
+            flag = false  
+        }
+
+        if(vDex == "")
+        {
+            document.getElementById("dexSpan").innerText = "Enter number 8 - 20"
+            document.getElementById("abilScores").style.width = "350px"
+            flag = false  
+        }
+
+        if(vCon == "")
+        {
+            document.getElementById("conSpan").innerText = "Enter number 8 - 20"
+            document.getElementById("abilScores").style.width = "350px"
+            flag = false  
+        }
+
+        if(vInt == "")
+        {
+            document.getElementById("intSpan").innerText = "Enter number 8 - 20"
+            document.getElementById("abilScores").style.width = "350px"
+            flag = false  
+        }
+
+        if(vWis == "")
+        {
+            document.getElementById("wisSpan").innerText = "Enter number 8 - 20"
+            document.getElementById("abilScores").style.width = "350px"
+            flag = false  
+        }
+
+        if(vCharisma == "")
+        {
+            document.getElementById("charismaSpan").innerText = "Enter number 8 - 20"
+            document.getElementById("abilScores").style.width = "350px"
+            flag = false  
+        }
+
+        return flag;
 }
 
 // Fetches form data
 function getPageInfo():PlayerCharacter
 {
-    let form  = new PlayerCharacter()
-    
+    let form = new PlayerCharacter()
+
     form.playerName = (<HTMLInputElement> document.getElementById("playerName")).value
     form.charName = (<HTMLInputElement> document.getElementById("charName")).value
     form.charLevel = parseInt((<HTMLInputElement> document.getElementById("charLevel")).value)
@@ -61,8 +169,8 @@ function getPageInfo():PlayerCharacter
     return form
 }
 
-function displayInfo(pageInfo)
+function displayInfo(form:PlayerCharacter):void
 {
-    
+
 }
 
